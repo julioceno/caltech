e563ccc77383256614bea4d80fcb327d05daeb51
julioceno <julioreplay@gmail.com>
2026-02-07 00:07:16 -0300

    feat: add BackToHomeLink component for navigation to the home page

---
+++ app/components/BackToHomeLink.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { IoIosArrowRoundBack } from "react-icons/io";
+
+export default function BackToHomeLink() {
+    return (
+        <Link 
+            href="/" 
+            className="inline-flex w-fit items-center gap-2 text-secondary font-inter font-bold underline hover:-translate-x-0.5 transition-all duration-200"
+        >
+            <IoIosArrowRoundBack size={24} className="text-secondary" />
+            <span>Voltar para página inicial</span>
+        </Link>
+
+    );
+}
