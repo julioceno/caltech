8bb02cbbb0a8e8b9ef0116324a3e234c9d2a0d76
julioceno <julioreplay@gmail.com>
2026-02-08 12:56:22 -0300

    feat: add priority attribute to Image component for improved loading performance

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,6 +31,7 @@ export default function Hero() {
                             src="/main.svg"
                             alt="Construção Caltech"
                             fill
+                            priority
                         />
                     </div>
                 </div>
